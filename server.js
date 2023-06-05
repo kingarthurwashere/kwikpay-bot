b181629c829d07e271947b94d53a8765636de0ac
mbaradza <mbaradza1@gmail.com>
2023-06-05 13:37:29 +0200

    Fix mongoose error

--- server.js
+++ server.js
@@ -7,17 +7,18 @@ const bot = require('./services/bot.js')
 const routes = require('./router');
 const cronServices = require('./services/cron.service');
 
-// Connecting to database
-mongoose.set('strictQuery',false);
-mongoose.connect(config.mongo.url, ).then(r =>{
-  console.log('Database Connected...');
-} ).catch( r => { console.log( 'Database Not Connected!!', r ) } );
+
 routes.register(app)
 // Update Pending Transactions
 cronServices.updateAfterEveryFiveMinutes();
 //cronServices.updateTest();
 // Listening to port
 app.listen(3000,async () => {
+  // Connecting to database
+mongoose.set('strictQuery',false);
+await mongoose.connect(config.mongo.url, ).then(r =>{
+  console.log('Database Connected...');
+} ).catch( r => { console.log( 'Database Not Connected!!', r ) } );
   console.log("Bot is running .....")
   console.log('Server running on localhost:3000');
 } );
