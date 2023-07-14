a2c17ef550521cd20ba40d07294f319a36036d52
kingarthurwashere <arthurnyasango1@outlook.com>
2023-07-14 22:21:55 +0200

    pesepay fix

--- services/pesepay.service.js
+++ services/pesepay.service.js
@@ -1,26 +1,25 @@
-const pesepay = require('pesepay');
-const config = require( '../config' );
-const pesepay = pesepay(config.INTEGRATION_KEY, config.ENCRYPTION_KEY);
+const config = require('../config');
+const axios = require('axios');
 
+async function checkout(chatId, user, transaction_id, service, amount, currency) {
+  const successUrl = `${config.redirect_url}/success?fname=${user}&chat_id=${chatId}&transaction=${transaction_id}&service=${service}`;
+  const failerUrl = `${config.redirect_url}/failure?fname=${user}&chat_id=${chatId}&transaction=${transaction_id}&service=${service}`;
 
-async function checkout(chatId,user,transaction_id,service) {
-  
-  const successUrl = `${config.redirect_url}/success?session_id={CHECKOUT_SESSION_ID}&fname=${user}&chat_id=${chatId}&transaction=${transaction_id}&service=${service}`
-  const failerUrl = `${config.redirect_url}/failure?session_id={CHECKOUT_SESSION_ID}&fname=${user}&chat_id=${chatId} &transaction=${transaction_id}&service=${service}`
-  const session = await pesepay.checkout.sessions.create({
-    line_items: [
-      {
-        price: config.SUBSCRIPTION_PRICE,
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: successUrl,
-    cancel_url: failerUrl,
+  try {
+    const response = await axios.post('https://pesepay.com/api/v1/payment', {
+      amount: amount,
+      currency: currency,
+      description: 'Payment for a product',
+      success_url: successUrl,
+      cancel_url: failerUrl,
+    });
 
-  });
+    const paymentUrl = response.data.paymentUrl;
+    return paymentUrl || null;
+  } catch (error) {
+    console.error('Error creating Pesepay payment:', error);
+    return null;
+  }
+}
 
-  return session.url||null
-};
-
-module.exports = { checkout};
+module.exports = { checkout };
