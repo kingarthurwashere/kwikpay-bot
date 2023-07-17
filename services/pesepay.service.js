37c0ecb046e89e66a7cc97da0a4efc80df517d7c
kingarthurwashere <arthurnyasango1@outlook.com>
2023-07-17 15:34:14 +0200

    pesepay issue

--- services/pesepay.service.js
+++ services/pesepay.service.js
@@ -1,20 +1,28 @@
+const { Pesepay } = require('pesepay');
 const config = require('../config');
 const axios = require('axios');
+
+// Create an instance of the Pesepay class using your integration key and encryption key
+const pesepay = new Pesepay(config.INTEGRATION_KEY, config.ENCRYPTION_KEY);
 
 async function checkout(chatId, fname, transactionId, service, amount, currency) {
   const successUrl = `${config.redirect_url}/success?fname=${fname}&chat_id=${chatId}&transaction=${transactionId}&service=${service}`;
   const failureUrl = `${config.redirect_url}/failure?fname=${fname}&chat_id=${chatId}&transaction=${transactionId}&service=${service}`;
 
+  // Set the return and result URLs
+  pesepay.returnUrl = successUrl;
+  pesepay.resultUrl = failureUrl;
+
   try {
-    const response = await axios.post('https://pesepay.com/api/v1/payment', {
+    // Create a Pesepay session
+    const session = await pesepay.createSession({
       amount: amount,
       currency: currency,
       description: 'Payment for a product',
-      success_url: successUrl,
-      cancel_url: failureUrl,
     });
 
-    const paymentUrl = response.data.paymentUrl;
+    // Get the payment URL from the session
+    const paymentUrl = session.paymentUrl;
     return paymentUrl;
   } catch (error) {
     console.error('Error generating Pesepay payment URL:', error);
@@ -23,3 +31,4 @@ async function checkout(chatId, fname, transactionId, service, amount, currency)
 }
 
 module.exports = { checkout };
+
