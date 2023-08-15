e48bb0dfe2ea6d9042c92a8f4ffd8adc31bb1d14
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-08-15 23:00:56 +0200

    fix: add app.use(express.json())

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-require('dotenv').config()
-const express = require( 'express' );
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const config = require( './config.js' )
-const mongoose = require( 'mongoose' );
-const bot = require('./services/bot.js')
-const routes = require('./router');
-const cronServices = require('./services/cron.service');
+const config = require("./config.js");
+const mongoose = require("mongoose");
+const bot = require("./services/bot.js");
+const routes = require("./router");
+const cronServices = require("./services/cron.service");
 
+app.use(express.json());
 
-routes.register(app)
+routes.register(app);
 // Update Pending Transactions
 cronServices.updateAfterEveryFiveMinutes();
 //cronServices.updateTest();
 // Listening to port
-app.listen(3000,async () => {
+app.listen(3000, async () => {
   // Connecting to database
-mongoose.set('strictQuery',false);
-await mongoose.connect(config.mongo.url, ).then(r =>{
-  console.log('Database Connected...');
-} ).catch( r => { console.log( 'Database Not Connected!!', r ) } );
-  console.log("Bot is running .....")
-  console.log('Server running on localhost:3000');
-} );
+  mongoose.set("strictQuery", false);
+  await mongoose
+    .connect(config.mongo.url)
+    .then((r) => {
+      console.log("Database Connected...");
+    })
+    .catch((r) => {
+      console.log("Database Not Connected!!", r);
+    });
+  console.log("Bot is running .....");
+  console.log("Server running on localhost:3000");
+});
 
 module.exports = app;
