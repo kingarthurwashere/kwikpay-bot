d09986070344b28a7910d5bb1a94814643eba3f5
kingarthurwashere <arthurnyasango1@outlook.com>
2023-09-19 15:02:38 +0200

    json

--- server.js
+++ server.js
@@ -3,25 +3,28 @@ const express = require( 'express' );
 const app = express();
 const config = require( './config.js' )
 const mongoose = require( 'mongoose' );
-const bot = require('./services/bot.js')
-const routes = require('./router');
-const cronServices = require('./services/cron.service');
+const bot = require( './services/bot.js' )
+const routes = require( './router' );
+const cronServices = require( './services/cron.service' );
 
-app.use(express.json());
+//Pesepay Json extractor
+app.use( express.json() );
 
-routes.register(app)
+routes.register( app )
 // Update Pending Transactions
 cronServices.updateAfterEveryFiveMinutes();
 //cronServices.updateTest();
 // Listening to port
-app.listen(3000,async () => {
+app.listen( 3000, async () =>
+{
   // Connecting to database
-mongoose.set('strictQuery',false);
-await mongoose.connect(config.mongo.url, ).then(r =>{
-  console.log('Database Connected...');
-} ).catch( r => { console.log( 'Database Not Connected!!', r ) } );
-  console.log("Bot is running .....")
-  console.log('Server running on localhost:3000');
+  mongoose.set( 'strictQuery', false );
+  await mongoose.connect( config.mongo.url, ).then( r =>
+  {
+    console.log( 'Database Connected...' );
+  } ).catch( r => { console.log( 'Database Not Connected!!', r ) } );
+  console.log( "Bot is running ....." )
+  console.log( 'Server running on localhost:3000' );
 } );
 
 module.exports = app;
